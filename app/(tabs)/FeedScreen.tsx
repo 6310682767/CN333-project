63093e86f2311a1ea920a403744354d3fd9fb77d
npybird <firstbird14120@gmail.com>
2025-04-19 01:27:07 +0700

    Working on Login Screen

---
+++ app/(tabs)/FeedScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function FeedScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-100">
+      <Text className="text-xl text-red-500">Welcome to the Feed Screen!</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-200">
+      <Text className="text-xl text-red-500">สวัสดี TU TALK!</Text>
+    </View>
+  );
+}
